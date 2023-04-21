4ed6aed32980a42b4fe9f6d81d4fd986a13b670c
Luthfan Prasasta <67474169+zettonaender@users.noreply.github.com>
2023-04-21 13:48:33 +0700

    Update okrange.py

--- okrange.py
+++ okrange.py
@@ -25,27 +25,27 @@ def startyourengine():
     print("Choose wav file: ", end='')
     x = int(input(''))
     x = listofwavs[x]
-    startfreq = input('Start frequency: ')
-    endfreq = input('End frequency: ')
+    startfreq = float(input('Start frequency: '))
+    endfreq = float(input('End frequency: '))
 
     tmp = helper.readWav(x+'.wav')
     if len(tmp) == 1:
         ir = tmp
         freq, arr = helper.getFrequencyResponse(ir, normalize=True)
         arr -= np.max(arr)
-        reduceby = getMaxInRange(freq, arr, float(startfreq), float(endfreq))
+        reduceby = getMaxInRange(freq, arr, startfreq, endfreq)
         arr = np.clip(arr, None, reduceby)
         arr -= 0.1
         ir = helper.createMinPhase(freq, arr, taplength=131072)
-        helper.writeWav(ir, f'myresultfir/{x}_{reduceby:.1f}.wav')
+        helper.writeWav(ir, f'myresultfir/{x}_{startfreq:.1f}_{endfreq:.1f}.wav')
     if len(tmp) == 2:
         ir1, ir2 = tmp[0], tmp[1]
         freq, arr1 = helper.getFrequencyResponse(ir1, normalize=False)
         freq, arr2 = helper.getFrequencyResponse(ir2, normalize=False)
         arr1 -= np.max(arr1)
         arr2 -= np.max(arr2)
-        max1 = getMaxInRange(freq, arr1, float(startfreq), float(endfreq))
-        max2 = getMaxInRange(freq, arr2, float(startfreq), float(endfreq))
+        max1 = getMaxInRange(freq, arr1, startfreq, endfreq)
+        max2 = getMaxInRange(freq, arr2, startfreq, endfreq)
         reduceby = max(max1, max2)
         arr1 = np.clip(arr1, None, reduceby)
         arr2 = np.clip(arr2, None, reduceby)
@@ -53,7 +53,7 @@ def startyourengine():
         arr2 -= 0.1
         ir1, ir2 = helper.createMinPhaseStereo(
             freq, arr1, arr2, taplength=131072)
-        helper.writeWavStereo(ir1, ir2, f'myresultfir/{x}_{reduceby:.1f}.wav')
+        helper.writeWavStereo(ir1, ir2, f'myresultfir/{x}_{startfreq:.1f}_{endfreq:.1f}.wav')
 
 
 startyourengine()
